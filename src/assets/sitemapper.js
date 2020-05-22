6c254320f97b5b0e03583be2a18c60bf2243bab1
Connor Langlois <connor.langlois@gmail.com>
2020-05-22 19:03:15 -0400

    Add user agent

--- src/assets/sitemapper.js
+++ src/assets/sitemapper.js
@@ -97,6 +97,9 @@ export default class Sitemapper {
       uri: url,
       resolveWithFullResponse: true,
       gzip: true,
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'
+      }
     };
 
     return new Promise((resolve) => {
