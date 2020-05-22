f84ef690fefff9bc252ba806710530908368a372
Connor Langlois <connor.langlois@gmail.com>
2020-05-22 19:19:18 -0400

    Build

--- src/examples/index.js
+++ src/examples/index.js
@@ -2,25 +2,29 @@ import Sitemapper from '../assets/sitemapper.js';
 
 const sitemapper = new Sitemapper();
 
-const Google = new Sitemapper({
-  url: 'https://www.google.com/work/sitemap.xml',
-  timeout: 15000, // 15 seconds
-});
+// const Google = new Sitemapper({
+//   url: 'https://www.google.com/work/sitemap.xml',
+//   timeout: 15000, // 15 seconds
+// });
 
-Google.fetch()
-  .then(data => console.log(data.sites))
-  .catch(error => console.log(error));
+// Google.fetch()
+//   .then(data => console.log(data.sites))
+//   .catch(error => console.log(error));
 
-sitemapper.timeout = 5000;
+// sitemapper.timeout = 5000;
 
-sitemapper.fetch('http://wp.seantburke.com/sitemap.xml')
-  .then(({ url, sites }) => console.log(`url:${url}`, 'sites:', sites))
-  .catch(error => console.log(error));
+// sitemapper.fetch('http://wp.seantburke.com/sitemap.xml')
+//   .then(({ url, sites }) => console.log(`url:${url}`, 'sites:', sites))
+//   .catch(error => console.log(error));
 
-sitemapper.fetch('http://www.cnn.com/sitemaps/sitemap-index.xml')
-  .then(data => console.log(data))
-  .catch(error => console.log(error));
+// sitemapper.fetch('http://www.cnn.com/sitemaps/sitemap-index.xml')
+//   .then(data => console.log(data))
+//   .catch(error => console.log(error));
 
-sitemapper.fetch('http://www.stubhub.com/new-sitemap/us/sitemap-US-en-index.xml')
+// sitemapper.fetch('http://www.stubhub.com/new-sitemap/us/sitemap-US-en-index.xml')
+//   .then((data) => console.log(data))
+//   .catch(error => console.log(error));
+
+sitemapper.fetch('https://onionplay.co/sitemaps.xml')
   .then((data) => console.log(data))
   .catch(error => console.log(error));
